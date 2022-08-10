bc4cada7b8da77bbe7a2443235b2f0cfc8785719
Harish mahur <hmahur20@gmail.com>
2022-08-10 07:54:24 +0530

    modified:finding maximum integer

--- Main.java
+++ Main.java
@@ -2,4 +2,14 @@ package com.generics;
 
 public class Main {
 
+	public static void main(String[] args) {
+		System.out.println("welcome to generics program");
+		MaxInteger maxInt = new MaxInteger();
+		System.out.println("Test case 1");
+		maxInt.maxvalue(7, 5, 6);
+		System.out.println("Test case 2");
+		maxInt.maxvalue(5, 6, 4);
+		System.out.println("Test case 3");
+		maxInt.maxvalue(5, 6, 8);
+	}
 }
